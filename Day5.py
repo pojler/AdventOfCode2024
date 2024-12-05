f11f2dd2302d7075d1d953c9c15444806f416817
pojler98@gmail.com <pojler98@gmail.com>
2024-12-06 00:14:08 +0100

    Day5 -beautifying code xD

--- Day5.py
+++ Day5.py
@@ -5,7 +5,7 @@ pageUpdates = [[int(elem) for elem in line.split(",")]for line in inputSplit[1].
 
 def validate (update):
     for i in range(len(update) -1 ):
-        if ([update[i], update[i+1]] not in ruleSet ) :
+        if ([update[i], update[i+1]] not in ruleSet):
                 return False
     return True
 
@@ -16,17 +16,18 @@ incorrectUpdates = []
 
 for update in pageUpdates :
     if(validate(update)):
-        sumOfMidElementsInCorrectUpdates  = sumOfMidElementsInCorrectUpdates + update[(int(len(update)/2))]
+        sumOfMidElementsInCorrectUpdates = sumOfMidElementsInCorrectUpdates + update[(int(len(update)/2))]
     else:
          incorrectUpdates.append(update)
 
 for update in incorrectUpdates:
     while(not validate(update)):
         for i in range(len(update) -1 ):
-            if ([update[i], update[i+1]] not in ruleSet ) :
+            if ([update[i], update[i+1]] not in ruleSet ):
                 temp = update[i+1]
                 update[i+1] = update[i]
                 update[i] = temp
     sumOfMidElementsInIncorrectUpdates  = sumOfMidElementsInIncorrectUpdates + update[(int(len(update)/2))]
+    
 print ("The sum of mid Page from correct update is: " + str(sumOfMidElementsInCorrectUpdates))
 print ("The sum of mid Page from incorrect update is: " + str(sumOfMidElementsInIncorrectUpdates))
